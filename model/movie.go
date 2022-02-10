e0145de857b32200e6fd907987503985dac31fcf
hao wang <2353784237@qq.com>
2022-02-10 14:46:39 +0800

    更新了模型

---
+++ model/movie.go
@@ -0,0 +1,15 @@
+package model
+
+type Movie struct {
+	Name string
+	Director string
+	Main_performer string
+	Type string
+	Country string
+	Language string
+	Date string
+	Length string
+	Stuff string
+	Id int
+	PicNum int
+}
